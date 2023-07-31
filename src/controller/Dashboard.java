ce9890a1846146f89070ae5537fccdd59094ac20
russelljewell <russelljewell@protonmail.com>
2023-07-31 09:06:16 -0600

    configure details panel methods

--- src/controller/Dashboard.java
+++ src/controller/Dashboard.java
@@ -1,9 +1,16 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import model.Appointment;
 
-public class Dashboard {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Dashboard implements Initializable {
     public TableView allAppointmentTable;
     public TableColumn allAppointmentIdColumn;
     public TableColumn allCustomerIdColumn;
@@ -65,30 +72,140 @@ public class Dashboard {
     public TextField endTextField;
 
     public void onActionCreateCustomer(ActionEvent actionEvent) {
-        detailsLabel.setText("Create Customer");
+        customerDetails();
     }
 
     public void onActionDeleteCustomer(ActionEvent actionEvent) {
+        initializeDetails();
     }
 
     public void onActionCreateAppointment(ActionEvent actionEvent) {
-        detailsLabel.setText("Create Appointment");
-
+        appointmentDetails();
     }
 
     public void onActionDeleteAppointment(ActionEvent actionEvent) {
+        initializeDetails();
     }
 
     public void onActionSave(ActionEvent actionEvent) {
+        if (!customerIdLabel.isVisible()) {
+
+        }
     }
 
     public void onActionClear(ActionEvent actionEvent) {
+        if (!customerIdLabel.isVisible()) {
+
+        }
     }
 
     public void onActionGenerateReport(ActionEvent actionEvent) {
     }
 
     public void onActionExit(ActionEvent actionEvent) {
+        System.exit(0);
     }
 
+    public void customerDetails() {
+        detailsLabel.setText("Create Customer");
+        customerIdLabel.setVisible(true);
+        customerIdText.setVisible(true);
+
+        customerNameLabel.setVisible(true);
+        customerNameLabel.setText("Customer Name");
+        customerNameTextField.setVisible(true);
+
+        addressLabel.setVisible(true);
+        addressLabel.setText("Address");
+        addressTextField.setVisible(true);
+
+        postalLabel.setVisible(true);
+        postalLabel.setText("Postal Code");
+        postalTextField.setVisible(true);
+
+        phoneLabel.setVisible(true);
+        phoneLabel.setText("Phone Number");
+        phoneTextField.setVisible(true);
+
+        countryLabel.setVisible(true);
+        countryLabel.setText("Country");
+        countryComboBox.setVisible(true);
+
+        divisionLabel.setVisible(true);
+        divisionLabel.setText("Division");
+        divisionComboBox.setVisible(true);
+    }
+
+    public void appointmentDetails() {
+        detailsLabel.setText("Create Appointment");
+        customerIdLabel.setVisible(true);
+        customerIdText.setVisible(true);
+
+        customerNameLabel.setVisible(true);
+        customerNameLabel.setText("Title");
+        customerNameTextField.setVisible(true);
+
+        appointmentIdLabel.setVisible(true);
+        appointmentIdText.setVisible(true);
+
+        addressLabel.setVisible(true);
+        addressLabel.setText("Description");
+        addressTextField.setVisible(true);
+
+        postalLabel.setVisible(true);
+        postalLabel.setText("Location");
+        postalTextField.setVisible(true);
+
+        phoneLabel.setVisible(true);
+        phoneLabel.setText("Type");
+        phoneTextField.setVisible(true);
+
+        countryLabel.setVisible(true);
+        countryLabel.setText("Contact");
+        countryComboBox.setVisible(true);
+
+        divisionLabel.setVisible(true);
+        divisionLabel.setText("User ID");
+        divisionComboBox.setVisible(true);
+
+        dateLabel.setVisible(true);
+        dateDatePicker.setVisible(true);
+
+        startLabel.setVisible(true);
+        startTextField.setVisible(true);
+
+        endLabel.setVisible(true);
+        endTextField.setVisible(true);
+    }
+
+    public void initializeDetails() {
+        detailsLabel.setText("Details");
+        customerIdLabel.setVisible(false);
+        customerIdText.setVisible(false);
+        customerNameLabel.setVisible(false);
+        customerNameTextField.setVisible(false);
+        appointmentIdLabel.setVisible(false);
+        appointmentIdText.setVisible(false);
+        addressLabel.setVisible(false);
+        addressTextField.setVisible(false);
+        postalLabel.setVisible(false);
+        postalTextField.setVisible(false);
+        phoneLabel.setVisible(false);
+        phoneTextField.setVisible(false);
+        countryLabel.setVisible(false);
+        countryComboBox.setVisible(false);
+        divisionLabel.setVisible(false);
+        divisionComboBox.setVisible(false);
+        dateLabel.setVisible(false);
+        dateDatePicker.setVisible(false);
+        startLabel.setVisible(false);
+        startTextField.setVisible(false);
+        endLabel.setVisible(false);
+        endTextField.setVisible(false);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+       initializeDetails();
+    }
 }

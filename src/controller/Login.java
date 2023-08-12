45172f8decafa2c91e1ceda97e7e26dd165ded77
russelljewell <russelljewell@protonmail.com>
2023-08-12 07:37:40 -0600

    create filewriter and log

--- src/controller/Login.java
+++ src/controller/Login.java
@@ -14,8 +14,11 @@ import model.User;
 import utilities.Alerts;
 import utilities.UserQuery;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.ResourceBundle;
 
 public class Login implements Initializable {
@@ -23,16 +26,20 @@ public class Login implements Initializable {
     public Text locationText;
     public TextField usernameTextField;
     public PasswordField passwordTextField;
-    Stage stage;
     boolean pass = false;
 
+
     public void onActionLogin(ActionEvent actionEvent) throws IOException {
+        FileWriter fw = new FileWriter("src/login_activity.txt", true);
+        PrintWriter pw = new PrintWriter(fw);
         for (User user : UserQuery.users()) {
             if (usernameTextField.getText().equals(user.getUserName()) && passwordTextField.getText().equals(user.getPassword())) {
                 pass = true;
             }
         }
         if (pass == true) {
+            pw.println("Successful Log-In: " + usernameTextField.getText() + " (" + LocalDateTime.now() + " UTC)");
+            pw.close();
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/dashboard.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
             Stage stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
@@ -40,6 +47,8 @@ public class Login implements Initializable {
             stage.centerOnScreen();
             stage.show();
         } else {
+            pw.println("Failed Log-In: " + usernameTextField.getText() + " (" + LocalDateTime.now() + " UTC)");
+            pw.close();
             Alerts.login();
         }
     }
